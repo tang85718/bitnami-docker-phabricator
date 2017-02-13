e27a69f9b4449504a3c905252d1ab9b88182e3b4
Sameer Naik <sameer@bitnami.com>
2017-02-13 18:15:33 +0530

    config: move default config var to Dockerfile

--- rootfs/app-entrypoint.sh
+++ rootfs/app-entrypoint.sh
@@ -14,19 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
-export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
-export PHABRICATOR_USERNAME=${PHABRICATOR_USERNAME:-"user"}
-export PHABRICATOR_PASSWORD=${PHABRICATOR_PASSWORD:-"bitnami1"}
-export PHABRICATOR_FIRSTNAME=${PHABRICATOR_FIRSTNAME:-"FirstName"}
-export PHABRICATOR_LASTNAME=${PHABRICATOR_LASTNAME:-"LastName"}
-export PHABRICATOR_HOST=${PHABRICATOR_HOST:-"127.0.0.1"}
-export PHABRICATOR_EMAIL=${PHABRICATOR_EMAIL:-"user@example.com"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
    for module in apache php phabricator; do
     initialize $module

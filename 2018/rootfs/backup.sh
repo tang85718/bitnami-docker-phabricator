6db457c984dee28cc76e4dd37a65c3455c30749b
dauglas <daulgas.tang@skytech.cc>
2018-02-02 10:37:59 +0800

    更新backup.sh
    增加scp命令和参数
    参数指定目标服务器

--- 2018/rootfs/backup.sh
+++ 2018/rootfs/backup.sh
@@ -1,4 +1,6 @@
 
+# 参数1: 示例tangxuyao@10.0.1.200:~/docker/phabricator
+
 cd /opt/bitnami/phabricator/bin
 ./storage dump | gzip > ~/backup-sql.gz
 
@@ -7,3 +9,5 @@ tar -zcvf ~/backup-data.tar.gz .
 
 cd /bitnami/phabricator/repo
 tar -zcvf ~/backup-repos.tar.gz .
+
+scp backup-* $1

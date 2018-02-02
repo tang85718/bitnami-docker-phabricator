4c95b45e683d2f0c8ce73d0a3bee9e476fb4874b
dauglas <daulgas.tang@skytech.cc>
2018-02-02 10:25:01 +0800

    修改备份脚本

--- 2018/rootfs/backup.sh
+++ 2018/rootfs/backup.sh
@@ -1,11 +1,9 @@
-now=`date +%Y-%m-%d`
-echo '当前日期:' $(now)
 
 cd /opt/bitnami/phabricator/bin
-./storage dump | gzip > ~/$(now).sql.gz
+./storage dump | gzip > ~/backup-sql.gz
 
 cd /bitnami/phabricator/data
-tar -zcvf ~/$(now)-data.tar.gz .
+tar -zcvf ~/backup-data.tar.gz .
 
 cd /bitnami/phabricator/repo
-tar -zcvf ~/$(now)-repos.tar.gz .
+tar -zcvf ~/backup-repos.tar.gz .

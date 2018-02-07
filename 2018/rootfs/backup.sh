6238dfebbf3b279dc8db165d681d6993a7d62864
tangxuyao <daulgas.tang@stringstech.co>
2018-02-07 14:35:53 +0800

    更新backup.sh

--- 2018/rootfs/backup.sh
+++ 2018/rootfs/backup.sh
@@ -1,5 +1,5 @@
 
-# 参数1: 示例tangxuyao@10.0.1.200:~/docker/phabricator
+# 参数1: 示例stringstechOA@10.0.1.120:~/docker/bitnami/backup
 
 cd /opt/bitnami/phabricator/bin
 ./storage dump | gzip > ~/backup-sql.gz
@@ -10,4 +10,5 @@ tar -zcvf ~/backup-data.tar.gz .
 cd /bitnami/phabricator/repo
 tar -zcvf ~/backup-repos.tar.gz .
 
+cd ~
 scp backup-* $1
